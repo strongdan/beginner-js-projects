24c27455009d80e7a8db1ab5603909bbf57824bf
Dan Strong <danielstrong@mac.com>
2018-02-09 08:33:09 -0900

    Update magic-eight-ball.js

--- magic-eight-ball.js
+++ magic-eight-ball.js
@@ -1,1 +1,36 @@
+let userName = prompt('Please enter your name', '');
+let userQuestion = prompt('Please enter your question:', '');
+let randomNumber = Math.floor(Math.random() + 8);
+let eightBall = '';
 
+userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
+
+switch(randomNumberr) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;  
+  case 2:
+    eightBall = 'Reply hazy. Try again';
+    break;  
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;  
+  case 4:
+    eightBall = 'Don\'t count on it';
+    break;  
+  case 5:
+    eightBall = 'My sources say no';
+    break;  
+  case 6:
+    eightBall = 'Outlook not so good';
+    break; 
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;  
+}
+
+console.log(`${userName}'s question: ${userQuestion}`);
+console.log(`Eight ball's answer: ${eightBall}`);
