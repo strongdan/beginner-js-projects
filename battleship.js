095f47ff10710732f8ea87342a4bb149f6a97e3c
Dan Strong <danielstrong@mac.com>
2018-02-09 21:28:03 -0900

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -1,15 +1,27 @@
-const	ships = { carrier : 5, 
-                battleship : 4, 
-                cruiser : 3,
-                submarine : 3, 
-                destroyer : 2 }
+const	ships = { 'carrier' : 5, 
+                'battleship' : 4, 
+                'cruiser' : 3,
+                'submarine' : 3, 
+                'destroyer' : 2 }
+
+let grid1 = [];
+let grid2 = [];
 
 let userTurn = 1; // users take turns => 1 = user1, 2 = user2
 
 let shipPositions = {}; // tracks which squares have a ship on them, and which user they belong to
 
-const setGrid = (rows, columns) => {
+const setGrid = (grid, rows, columns) => {
   // creates space for ships based on user's preference, not more than 10 x 10 and not less than 5 x 5
+  // updates and returns grid variable
+}
+
+const showGrid = (shipPositions, grid) => {
+  // print grid with ships to console 
+}
+
+const shipsRemaining = () => {
+  // list remaining ships and their length 
 }
 
 const placeShip = (ship, startNum, endNum, startAlpha, endAlpha) => {
@@ -17,7 +29,7 @@ const placeShip = (ship, startNum, endNum, startAlpha, endAlpha) => {
   // checks whether ship is already present and ensures ship doesn't extend off board
 }
 
-const fireShots = (alpha, numeric) => {
+const fireShots = (x, y) => {
   // allows user to select position on board to fire at 
 }
 
@@ -30,7 +42,9 @@ const sunkShip = () => {
   // checks whether entire ship has been hit and alerts user  
 }
 
-
+const gameWon = () => {
+  // checks whether game has been won and prints winner
+}
 
 const playGame = () => {
   // starts game play 
