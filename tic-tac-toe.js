3e28db2be3475102f84abce0a80c4c09bb81bb14
Dan Strong <danielstrong@mac.com>
2018-02-10 13:34:38 -0900

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,22 @@
+let board = [ [' ', ' ', ' '],
+              [' ', ' ', ' '],
+              [' ', ' ', ' '] ];
+
+const printBoardPositions = () => {
+  console.log('Here is the current board...');
+  // prints positions for user
+  let currentNumber = 1;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+        board[i][j] = currentNumber;
+        currentNumber += 1;
+    }
+  }
+ console.log(board);
+}
+
+const placeMarker = (marker, position) => {
+  // adds X or O to board at specified location  
+}
+
 
