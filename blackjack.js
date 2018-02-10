9e465aeca842bebe3897189f344f069717105aa9
Dan Strong <danielstrong@mac.com>
2018-02-10 13:06:35 -0900

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -1,1 +1,25 @@
+let currentScore = 0;
+const cards = {'two' : 2,
+               'three' : 3,
+               'four' : 4,
+               'five' : 5,
+               'six' : 6,
+               'seven' : 7,
+               'eight' : 8,
+               'nine' : 9,
+               'ten' : 10,
+               'jack' : 10,
+               'queen' : 10,
+               'king' : 10,
+               'ace' : [1, 11] }
 
+const userPlay = () => {
+  currentScore += prompt('');  
+}
+
+
+const startGame = () => {
+  // starts game  
+}
+
+startGame();
