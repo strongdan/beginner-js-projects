a07805a70877bc2a8c34fc434938c4ebd48e0c99
Dan Strong <danielstrong@mac.com>
2018-02-10 13:23:06 -0900

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -13,13 +13,37 @@ const cards = {'two' : 2,
                'king' : 10,
                'ace' : [1, 11] }
 
-const userPlay = () => {
-  currentScore += prompt('');  
+const randomCard = (cards) => {
+  // returns random element from cards as an array
+  let card1 = Math.floor(Math.random())
+}
+
+const initialDeal = () => {
+  // deals two cards to start the game
+  card1 = randomCard(cards);
+  card2 = randomCard(cards);
+  // returns object with card names and values
+  return { card1, card2 };
+}
+
+const dealCard = () => {
+  // if user requests additional card, card is drawn and score is incremented
+  let card = randomCard(cards);
+  // returns card and value
+}
+
+const playGame = () => {
+  firstCards = initialDeal(cards);
+  currentScore += firstCards[0].val + firstCards[1].val;
+  console.log(`Your first cards are: ${firstCards[0]} and ${firstCards[1]});
+  console.log(`Your score is ${currentScore});
+  console.log('Welcome to blackjack!');
+  cards = initialDeal();
+  console.log();
+  
 }
 
 
-const startGame = () => {
-  // starts game  
-}
 
-startGame();
+
+playGame();
