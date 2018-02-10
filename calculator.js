e9669a6f7f3dc54dde99fd83aa1c61822f93d70d
Dan Strong <danielstrong@mac.com>
2018-02-09 21:45:55 -0900

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
 // allows basic arithmetic operations - including addition, subtraction, multiplication and division
 const add = (num1, num2) => {
-  return num1 + num2;
+  return Number(num1) + Number(num2);
 }
 
 const subtract = (num1, num2) => {
-  return num1 - num2;
+  return Number(num1) - Number(num2);
 }
 
 const multiply = (num1, num2) => {
-  return num1 * num2;
+  return Number(num1) * Number(num2);
 }
 
 const divide = (num1, num2) => {
-  if (num2 === 0) {
+  if (Number(num2) === 0) {
     console.log('You cannot divide by zero');  
   } else {
-    return num1 / num2;  
+    return Number(num1) / Number(num2);  
   }
 }
 
@@ -45,3 +45,4 @@ const calculate = () => {
     }
 }
 
+calculate();
