10b4976deebb77ef9aa90dce35204a469c0af0ef
Dan Strong <danielstrong@mac.com>
2018-02-11 09:54:58 -0900

    Create digital-clock.js

---
+++ digital-clock.js
@@ -0,0 +1,15 @@
+const standardOrTwentyFourHour = () => {
+  // prompts user for preference of clock format - 24hr or standard
+}
+
+const getCurrentTime = () => {
+  // gets current time and returns it in datetime format
+  let now = new Date();
+  return now;
+}
+
+const displayClock = () => {
+  // appends clock digits to main div in window
+}
+
+displayClock();
