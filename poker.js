9976575764ec79dd9b7513a37d7fcaffe4dde0c2
Dan Strong <danielstrong@mac.com>
2018-02-10 16:08:42 -0900

    Update poker.js

--- poker.js
+++ poker.js
@@ -1,1 +1,25 @@
+const cards = { hearts: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ],
+                diamonds: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ],
+                spades: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ],
+                clubs: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ] }
 
+const hands = [ 'Royal flush',
+                'Straight flush',
+                'Four of a kind',
+                'Full house',
+                'Flush',
+                'Straight',
+                'Three of a kind',
+                'Two pair',
+                'Pair',
+                'High Card' ]
+
+const ante = () => {
+  // allows players to ante  
+}
+
+const playGame = () => {
+  // starts game and plays one hand 
+}
+
+playGame();
