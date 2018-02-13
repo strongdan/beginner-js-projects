3a567bfb7906cb5b30c2adbe39405ee00505966c
Dan Strong <danielstrong@mac.com>
2018-02-12 15:15:20 -0900

    Update scrabble.js

--- scrabble.js
+++ scrabble.js
@@ -1,1 +1,13 @@
+const letterValues = {  'A' : 1, 'E' : 1, 'I' : 1, 'O' : 1, 'U' : 1, 'L' : 1, 'N' : 1, 'S' : 1, 'T' : 1, 'R' : 1,
+                        'D' : 2, 'G' : 2,
+                        'B' : 3, 'C' : 3, 'M' : 3, 'P' : 3,
+                        'F' : 4, 'H' : 4, 'V' : 4, 'W' : 4, 'Y' : 4,
+                        'K' : 5,
+                        'J' : 8, 'X' : 8,
+                        'Q' : 10, 'Z' : 10 };
 
+let letters = [];
+
+const dealLetters = () => {
+  // deals five letter from letters array to each player (if available) 
+}
