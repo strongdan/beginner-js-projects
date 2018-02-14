d4ab8464b8e022cc6de763b3a8b8d4fd5cfa0b55
Dan Strong <danielstrong@mac.com>
2018-02-14 11:17:05 -0900

    Update checkers.js

--- checkers.js
+++ checkers.js
@@ -7,8 +7,31 @@ let gameBoard = [ [  0,  1,  0,  1,  0,  1,  0,  1 ],
                   [  0,  2,  0,  2,  0,  2,  0,  2 ],
                   [  2,  0,  2,  0,  2,  0,  2,  0 ] ];
 
+let userOneCheckers = [];
+let userTwoCheckers = [];
+
 const startGame = () => {
   // set up game board in starting position  
+}
+
+const printBoard = () => {
+  // prints board with alphanumeric grid positions on the side and bottom 
+}
+
+const moveToken = () => {
+  // allows user to move a token to a grid position 
+}
+
+const validMove = () => {
+  // checks to see if move is valid 
+}
+
+const attack = () => {
+  // if user jumps other user's piece(s) those pieces are removed from the board 
+}
+
+const kingMe = () => {
+  // if token reaches the end of the board, it becomes a king 
 }
 
 const playGame = () => {
