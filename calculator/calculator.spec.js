234f4098efc6ccb93d8516e476e31ed13d100461
Dan Strong <danielstrong@mac.com>
2018-03-02 14:58:04 -0900

    Update calculator.spec.js

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,13 +1,25 @@
 require('./calculator.js');
 
-calculate
+describe('add', function() {
+    it('adds two numbers', function() {
+    expect(add(10, 42)).toEqual(52);
+  });
+});
   
-getUserInput
+describe('subtract', function() {
+    it('subtracts two numbers', function() {
+    expect(subtract(10, 42)).toEqual(-38);
+  });
+});
 
-add
+describe('multiply', function() {
+    it('multiplies two numbers', function() {
+    expect(multiply(10, 42)).toEqual(420);
+  });
+});
 
-subtract
-
-multiply
-
-divide
+describe('divide', function() {
+    it('divides two numbers', function() {
+    expect(divide(10, 42)).toEqual(0.23809523809523808);
+  });
+});
