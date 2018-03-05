157957f595cd3aeb89a653ec541e98611a8c70d8
Dan Strong <danielstrong@mac.com>
2018-03-05 08:03:36 -0900

    Update calculator.spec.js

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -5,10 +5,22 @@ describe('add', function() {
     expect(add(10, 42)).toEqual(52);
   });
 });
+
+describe('add', function() {
+    it('adds two numbers', function() {
+    expect(add(101, 5)).toEqual(106);
+  });
+});
   
 describe('subtract', function() {
     it('subtracts two numbers', function() {
     expect(subtract(10, 42)).toEqual(-38);
+  });
+});
+
+describe('subtract', function() {
+    it('subtracts two numbers', function() {
+    expect(subtract(42, 10)).toEqual(32);
   });
 });
 
@@ -18,9 +30,21 @@ describe('multiply', function() {
   });
 });
 
+describe('multiply', function() {
+    it('multiplies two numbers', function() {
+    expect(multiply(101, 5)).toEqual(505);
+  });
+});
+
 describe('divide', function() {
     it('divides two numbers', function() {
     expect(divide(10, 42)).toEqual(0.23809523809523808);
+  });
+});
+
+describe('divide', function() {
+    it('divides two numbers', function() {
+    expect(divide(42, 10)).toEqual(4.2);
   });
 });
 
