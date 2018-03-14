b37e58468623875b95b5cc6a1c37ca0d637bfed4
Dan Strong <danielstrong@mac.com>
2018-03-14 08:37:44 -0800

    Update checkers.js

--- checkers/checkers.js
+++ checkers/checkers.js
@@ -1,42 +1,75 @@
-let gameBoard = [ [  0,  1,  0,  1,  0,  1,  0,  1 ],
-                  [  1,  0,  1,  0,  1,  0,  1,  0 ],
-                  [  0,  1,  0,  1,  0,  1,  0,  1 ],
-                  [  0,  0,  0,  0,  0,  0,  0,  0 ],
-                  [  0,  0,  0,  0,  0,  0,  0,  0 ],
-                  [  2,  0,  2,  0,  2,  0,  2,  0 ],
-                  [  0,  2,  0,  2,  0,  2,  0,  2 ],
-                  [  2,  0,  2,  0,  2,  0,  2,  0 ] ];
+let startingBoard = [ [  0,  1,  0,  1,  0,  1,  0,  1 ],
+                    [  1,  0,  1,  0,  1,  0,  1,  0 ],
+                    [  0,  1,  0,  1,  0,  1,  0,  1 ],
+                    [  0,  0,  0,  0,  0,  0,  0,  0 ],
+                    [  0,  0,  0,  0,  0,  0,  0,  0 ],
+                    [  2,  0,  2,  0,  2,  0,  2,  0 ],
+                    [  0,  2,  0,  2,  0,  2,  0,  2 ],
+                    [  2,  0,  2,  0,  2,  0,  2,  0 ] ];
 
 let userOneCheckers = [];
 let userTwoCheckers = [];
 
 const startGame = () => {
   // set up game board in starting position  
+  let board = startingBoard;
+  return board;
 }
 
-const printBoard = () => {
+const firstUser = () => {
+  // returns first user randomly
+  let user = Math.floor(Math.random() * 2) + 1 
+  return user;
+}
+
+const printBoard = (board) => {
   // prints board with alphanumeric grid positions on the side and bottom 
+  board.forEach(function(row){
+    console.log(row);
+  });
 }
 
 const moveToken = () => {
   // allows user to move a token to a grid position 
+  
+}
+
+const jumpAvailable = (user, boardPosition) => {
+  // checks if a user has a jump available at a specific position
 }
 
 const validMove = () => {
   // checks to see if move is valid 
+  // if a jump is available, user must take it
 }
 
 const attack = () => {
-  // if user jumps other user's piece(s) those pieces are removed from the board 
+  // jumps other user's piece(s) and removes those pieces from the board 
 }
 
-const kingMe = () => {
+const kingMe = (user, boardYPosition, boardXPosition) => {
   // if token reaches the end of the board, it becomes a king 
+  if (user === 1 && boardYPosition === 7) {
+      // token is changed from a number to a "K"
+      board[boardYPosition][boardXPosition] = 'K1';
+  } else if (user === 2 && boardYPosition === 0) {
+      board[boardYPosition][boardXPosition] = 'K2';  
+  }
+}
+
+const switchUser = (currentUser) => {
+  if (currentUser === 1) {
+    return 2; 
+  } else {
+    return 1;
+  }
 }
 
 const playGame = () => {
   // start game and play through 
-  startGame();
+  printBoard(startGame);
+  console.log(`User ${firstUser()} goes first!`);
+  
 }
 
 playGame();
