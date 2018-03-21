815193096047e8ce524b4fb2a013f8e47f1a4c32
Dan Strong <danielstrong@mac.com>
2018-03-20 22:03:51 -0800

    Update choose-your-own-adventure.js

--- adventure/choose-your-own-adventure.js
+++ adventure/choose-your-own-adventure.js
@@ -1,1 +1,14 @@
 
+
+const printStory = () => {
+  // present user with storyline based on their choice
+}
+
+const queryUser = () => {
+  // ask user how they would like to proceed 
+}
+
+const startGame = () => {
+  // initiate game play
+  printFirstStory();
+}
