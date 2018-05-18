b4df121ebf6c4bfa86b0624a430b27941393e609
Dan Strong <danielstrong@mac.com>
2018-05-18 08:39:10 -0800

    Update snake.js

--- snakeGame/snake.js
+++ snakeGame/snake.js
@@ -1,1 +1,23 @@
+class Snake {
+  constructor() {
+    this.pos = createVector( 0,0 );
+  }
+  
+    moveDir(){
+    }
+    
+    eatsFood(){ 
+    }
 
+    losesTail(){
+    }
+
+    update(){
+    }
+
+    show(){
+      fill(255);
+      rect(this.pos.x, this.pos.y, SCL, SCL);
+    }
+  
+}
