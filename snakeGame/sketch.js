328b72d8a377d46d1b932e9efe78113d0f760daa
Dan Strong <danielstrong@mac.com>
2018-05-19 21:23:02 -0800

    Update sketch.js

--- snakeGame/sketch.js
+++ snakeGame/sketch.js
@@ -11,3 +11,17 @@ function draw(){
   snake.show();
 }  
 
+function keyPressed(){
+  if (keyCode === UP_ARROW) {
+    moveDir(0, -1);
+  }
+  else if (keyCode === DOWN_ARROW) {
+    moveDir(0, 1);
+  }
+  else if (keyCode === LEFT_ARROW) {
+    moveDir(-1, 0);
+  }
+  else if (keyCode === RIGHT_ARROW) {
+    moveDir(1, 0);
+  }  
+}
